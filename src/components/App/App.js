36279554f19656bda246db8055067d354c832614
Julia Tushchenko <84866878+Julia-Tuschenko@users.noreply.github.com>
2022-02-01 21:53:18 +0200

    fix

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -106,31 +106,3 @@ App.propTypes = {
   showModal: PropTypes.bool,
   isLoading: PropTypes.bool,
 };
-
-// export default class App extends Component {
-//   state = {
-//     art: null,
-//     loading: false,
-//   }
-
-//   componentDidMount(){
-//     // const BASE_URL = `https://pixabay.com/api/`;
-//     // const KEY = '24399696-8e36fcdd9504681aa333f9a82';
-//     this.setState({ loading: true });
-//     fetch('https://pixabay.com/api/?q=cat&page=1&key=24399696-8e36fcdd9504681aa333f9a82&image_type=photo&orientation=horizontal&per_page=20')
-//     .then(res => res.json())
-//     .then(art => this.setState({art}))
-//     .finally(() => this.setState({ loading: false }));
-
-//   }
-
-//   render(){
-//     return (
-//       <div >
-//          <Searchbar />
-//          {this.state.loading && <h1>Загрузка...</h1>}
-//          {this.state.art && (<div>{this.state.art.tags}</div>)}
-//       </div>
-//     );
-//   }
-// }
